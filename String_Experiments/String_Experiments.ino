543d71f0e1b8a0616a9d70d54f68d7d001d72e5b
Vishnu M Aiea <vishnumaiea@gmail.com>
2018-11-19 18:10:39 +0530

    Added more operations

--- String_Experiments/String_Experiments.ino
+++ String_Experiments/String_Experiments.ino
@@ -8,16 +8,54 @@
 //  with FreeRTOS on STM32 Nucleo boards. I was unable to create String
 //  objects or pass them to functions.
 //
-//  Last modified : +05:30 12:23:07 PM, 18-11-2018, Sunday
+//  Last modified : +05:30 6:06:16 PM, 19-11-2018, Monday
 //
 //  Web : https://github.com/vishnumaiea/STM32-Nucleo-Snippets
 //
 //========================================================================//
 
 #include <string>
+// #include <sstream>
 #include <STM32FreeRTOS.h> //you need to install this library
 
 using namespace std;
+
+//========================================================================//
+//converts a long integer to string object
+
+// string intToString (long long int i) { //this works but need lot of memory
+//   string out_string;
+//   stringstream someStream;
+//   someStream << i;
+//   out_string = someStream.str();
+//   return out_string;
+// }
+
+string intToString (long long int n) {
+  char out_string [40];
+  // sprintf(out_string, "%d", n);
+  itoa(n, out_string, 10); //default base is 10
+  return string(out_string);
+}
+
+//========================================================================//
+//converts a long integer to string object
+
+string intToString (long long int n, int base) {
+  char out_string [40];
+  // sprintf(out_string, "%d", n);
+  itoa(n, out_string, base);
+  return string(out_string);
+}
+
+//========================================================================//
+//converts a double number to string object
+
+string floatToString (double n, int w, int p) { //number, min width, precision
+  char charBuffer[40] = {0};
+  // return string(dtostrf(12345564.2114122222, 6, 10, charBuffer)); //an example
+  return string(dtostrf(n, w, p, charBuffer));
+}
 
 //========================================================================//
 //create a class
@@ -56,14 +94,46 @@ void stringHandlers::stringHandler_2 (string *S) {
 
 void stringHandlers::stringHandler_2 (string S, int i) {
   Serial.println("String Handler 2 OL");
-  Serial.print("String object received = ");
-  Serial.println(S.c_str()); //we have a copy of an object
+  Serial.print("String converted to C_String = ");
+  // Serial.println(S.c_str()); //we have a copy of an object
+  char stringBuffer[S.length()+1]; //one additional byte for NULL
+  strcpy(stringBuffer, S.c_str()); //this works fine
+  Serial.println(stringBuffer);
+  Serial.print("Array size = ");
+  Serial.println(sizeof(stringBuffer));
   Serial.print("Integer received = ");
   Serial.println(i);
+
+  Serial.println();
+  Serial.println("String Insert Operation");
+  string firstString, secondString;
+  firstString = "Hello";
+  secondString = "World";
+  firstString.insert(firstString.length(), " World"); //method 1
+  // secondString.insert(0, firstString); //method 2
+  Serial.println(firstString.c_str());
+
+  Serial.println();
+  Serial.println("String Addition");
+  string thirdString;
+  thirdString = string("Hello ") + string("World");
+  Serial.println(thirdString.c_str());
+
+  Serial.println();
+  Serial.println("Integer to String Conversion");
+  string fourthString = intToString(-888888888, 2);
+  // string fourthString = to_string(1234567890123); //this has limitations
+  Serial.println(fourthString.c_str());
+
+  Serial.println();
+  Serial.println("Float to String Conversion");
+  string fifthString = floatToString(3.141592, 8, 6);
+  Serial.println(fifthString.c_str());
   Serial.println();
 }
 
 //========================================================================//
+//a stand-alone function
 
 void stringHandler_3 (string *S) {
   Serial.println("String Handler 3");
